cb6c65dfb35a44921f816c5e7a74b5891b1645b3
Sadick14 <issakasaddick14@gmail.com>
2026-06-06 17:33:31 +0000

    fix: remove stray character crashing register page build

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -9,7 +9,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { login, home } from '@/routes';
-i
+
 
 defineOptions({ layout: null });
 
